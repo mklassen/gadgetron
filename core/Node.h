113b25f28a2b6dfae7fda2ca89f2551a4e8f0330
Martyn Klassen <lmklassen@gmail.com>
2023-07-03 08:15:12 -0400

    Make IsmrmrdHeader reference to shared object

--- core/Node.h
+++ core/Node.h
@@ -24,12 +24,12 @@ namespace Gadgetron::Core {
 
     class GenericChannelGadget : public Node, public PropertyMixin {
     public:
-        GenericChannelGadget(const Context& context, const GadgetProperties& properties) : PropertyMixin(properties), header{context.header} {}
+        GenericChannelGadget(const Context& context, const GadgetProperties& properties) : PropertyMixin(properties), header(context.header) {}
 
 //        [[deprecated("ChannelGadget should be called with both context and properties")]]
 //        GenericChannelGadget(const GadgetProperties& properties) : PropertyMixin(properties) {}
     protected:
-        const ISMRMRD::IsmrmrdHeader header ={};
+        const ISMRMRD::IsmrmrdHeader &header;
     };
 
     /**
